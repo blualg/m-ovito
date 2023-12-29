4e2ec3407718aec345ca6cc15e2c24f1c925eed2
Alexander Stukowski <stukowski@ovito.org>
2023-12-29 10:41:52 +0100

    Bumped version number to 3.11

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -24,7 +24,7 @@
 
 # This is the canonical program version number:
 SET(OVITO_VERSION_MAJOR         "3")
-SET(OVITO_VERSION_MINOR         "10")
+SET(OVITO_VERSION_MINOR         "11")
 SET(OVITO_VERSION_REVISION      "0")
 
 # Increment the following version counter every time the .ovito file format
