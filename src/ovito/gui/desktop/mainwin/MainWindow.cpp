aa7a3af4e3b43789e5ebcecdfa41be68b28c1095
Alexander Stukowski <stukowski@ovito.org>
2026-02-17 13:06:43 +0100

    Add icon to "Request a Feature" UI action

--- src/ovito/gui/desktop/mainwin/MainWindow.cpp
+++ src/ovito/gui/desktop/mainwin/MainWindow.cpp
@@ -363,6 +363,7 @@ void MainWindow::createMainMenu()
     helpMenu->setObjectName(QStringLiteral("HelpMenu"));
     helpMenu->addAction(actionManager()->getAction(ACTION_HELP_SHOW_ONLINE_HELP));
     helpMenu->addAction(actionManager()->getAction(ACTION_HELP_SHOW_SCRIPTING_HELP));
+    helpMenu->addSeparator();
     helpMenu->addAction(actionManager()->getAction(ACTION_HELP_REQUEST_FEATURE));
     helpMenu->addSeparator();
     helpMenu->addAction(actionManager()->getAction(ACTION_HELP_GRAPHICS_SYSINFO));
